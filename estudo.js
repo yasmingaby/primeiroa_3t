1036aeb31bc0932dc9ae0666cd85049a7f6dcb61
yasmingaby <108940329+yasmingaby@users.noreply.github.com>
2022-10-05 08:20:31 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -15,3 +15,19 @@ function escreva(){
     document.write(tabuada + "x 10 ="+(tabuada*10)+"<br>");
 document.write("Feito por " + nome )
 }
+
+function minhatabuada(){
+  for(let i = 1; i <= 10; i++){
+      document.write( "Tabuada do " + i + "<br>");
+    for(let j = 1; j <= 10; j++){
+        document.write(i + " x " + j + " = "+(j*i)+"<br>"); 
+    }
+     
+  }
+} 
+
+function quadrado(){
+    for(let i = 1; i <= 20; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+ "<br>")
+    }
+}
