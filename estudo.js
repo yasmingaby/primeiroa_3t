6ac03abd61a93182ed7bd2aae8a8188ef21bd510
yasmingaby <108940329+yasmingaby@users.noreply.github.com>
2022-10-19 08:24:40 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -32,3 +32,40 @@ function quadrado(){
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+let op = "";
+function operacao(ope){
+    op = ope;
+}
+function calcule(){
+let v1 = document.getElementById("v1").value;
+let v2 = document.getElementById("v2").value;
+let r = 0;
+
+if(op == "+"){
+    r = Number(v1) + Number(v2) ;
+}
+
+document.getElementById("resultado").innerHTML = r;
+
+}
+
+if(op == "-"){
+    r = Number(v1) - Number(v2) ;
+}
+
+
+
+
